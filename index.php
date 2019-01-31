2dc55653c72270b5488ec47cba27d4949687b7fd
Camila <camila.perellon@gmail.com>
2019-01-31 11:39:08 -0200

    Métodos de LIsta

--- index.php
+++ index.php
@@ -6,14 +6,27 @@ error_reporting(E_ALL);
 require_once("config.php");
 
 /*$sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
 
-$usuario = new Usuario();
-
+//carrega um usuario
+/*$usuario = new Usuario();
 $usuario->loadById(3);
+echo $usuario;
+*/
 
+//carrega uma lista de usuários
+/*$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+//carrega uma lista de usuarios buscando pelo login
+/*$search = Usuario::search("ca");
+echo json_encode($search);
+*/
+
+//Autentica usuarios
+$usuario = new Usuario();
+$usuario->login("camila", "12315");
 echo $usuario;
